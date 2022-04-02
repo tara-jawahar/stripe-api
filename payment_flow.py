484fd6f22fefd807c53d72bc33899f442bed0b67
tara-jawahar <aratj04@gmail.com>
2022-04-02 13:43:41 -0500

    add payment flow

---
+++ payment_flow.py
@@ -0,0 +1,15 @@
+# Set your secret key. Remember to switch to your live secret key in production.
+# See your keys here: https://dashboard.stripe.com/apikeys
+import stripe
+import os
+stripe.api_key = os.environ['STRIPE_API_KEY']
+
+stripe.Customer.create(description="My First Test Customer")
+
+stripe.PaymentIntent.create(
+  customer='{{CUSTOMER_ID}}',
+  currency="usd",
+  amount=2000,
+  payment_method_types=["card"],
+  setup_future_usage="on_session",
+)
